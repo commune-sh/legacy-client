9ead00172012fec5008559c15ba8b7b5a31fe88f
hummingbard <mail@hummingbard.com>
2023-05-25 21:22:45 +0300

    tweak mobile vf, position fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -196,6 +196,15 @@ function collapse() {
 
 
 @media screen and (max-width: 768px) {
+    .root {
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        height: 100vh;
+        width: 100vw;
+    }
     .container {
         grid-template-columns: auto;
     }
