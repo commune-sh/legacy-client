3579aa43d3c3b20d70430fa619c5cdc41822735b
harshmangalam <harshdev8218@gmail.com>
2023-10-01 22:20:42 +0530

    added PWA support

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,123 +1,142 @@
 <script>
-import '/static/css/main.css'
-import Authentication from '$lib/auth/authentication.svelte'
-import Switcher from '$lib/switcher/switcher.svelte'
-import Sidebar from '$lib/sidebar/sidebar.svelte'
-import { browser } from '$app/environment';
-import { PUBLIC_BASE_URL, PUBLIC_META_TITLE, PUBLIC_MEDIA_URL, PUBLIC_FAVICON, PUBLIC_META_IMAGE, PUBLIC_INDEX, PUBLIC_META_DESCRIPTION } from '$env/static/public';
-import { onMount, tick } from 'svelte'
-import { page } from '$app/stores';
-import { store } from '$lib/store/store.js'
-import View from '$lib/view/view.svelte'
-import Sync from '$lib/sync/sync.svelte'
-import Health from '$lib/sync/health.svelte'
-import Down from '$lib/errors/down.svelte'
-import CreateSpace from '$lib/space/create-space.svelte'
-import Modal from '$lib/modal/modal.svelte'
-import Alert from '$lib/alert/alert.svelte'
-import About from '$lib/about/about.svelte'
-import DiscoverSpaces from '$lib/discover/discover.svelte'
-import NotFound from '$lib/errors/not-found.svelte'
-import Gallery from '$lib/gallery/gallery.svelte'
-import Settings from '$lib/settings/settings.svelte'
+  import "/static/css/main.css";
+  import Authentication from "$lib/auth/authentication.svelte";
+  import Switcher from "$lib/switcher/switcher.svelte";
+  import Sidebar from "$lib/sidebar/sidebar.svelte";
+  import { browser } from "$app/environment";
+  import {
+    PUBLIC_BASE_URL,
+    PUBLIC_META_TITLE,
+    PUBLIC_MEDIA_URL,
+    PUBLIC_FAVICON,
+    PUBLIC_META_IMAGE,
+    PUBLIC_INDEX,
+    PUBLIC_META_DESCRIPTION,
+  } from "$env/static/public";
+  import { onMount, tick } from "svelte";
+  import { page } from "$app/stores";
+  import { store } from "$lib/store/store.js";
+  import View from "$lib/view/view.svelte";
+  import Sync from "$lib/sync/sync.svelte";
+  import Health from "$lib/sync/health.svelte";
+  import Down from "$lib/errors/down.svelte";
+  import CreateSpace from "$lib/space/create-space.svelte";
+  import Modal from "$lib/modal/modal.svelte";
+  import Alert from "$lib/alert/alert.svelte";
+  import About from "$lib/about/about.svelte";
+  import DiscoverSpaces from "$lib/discover/discover.svelte";
+  import NotFound from "$lib/errors/not-found.svelte";
+  import Gallery from "$lib/gallery/gallery.svelte";
+  import Settings from "$lib/settings/settings.svelte";
+  import { pwaInfo } from "virtual:pwa-info";
 
-export let data;
-
-$: if(data) {
-    console.log(data)
-}
-
-
-$: if(data.health) {
-    $store.health = data.health
-}
-
-$: if(data?.state?.space) {
-    store.addSpaceState($page.params?.space, data.state)
-    store.stateReady()
-}
-
-$: if(data?.events) {
-    let roomID = data.events[0]?.room_id
-    if(isIndex) {
-        roomID = "index"
+  onMount(async () => {
+    if (pwaInfo) {
+      // @ts-ignore
+      const { registerSW } = await import("virtual:pwa-register");
+      registerSW({
+        immediate: true,
+        onRegistered(r) {
+          console.log(`SW Registered: ${r}`);
+        },
+        onRegisterError(error) {
+          console.log("SW registration error", error);
+        },
+      });
     }
-    if(isAll) {
-        roomID = "all"
+  });
+  $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : "";
+
+  export let data;
+
+  $: if (data) {
+    console.log(data);
+  }
+
+  $: if (data.health) {
+    $store.health = data.health;
+  }
+
+  $: if (data?.state?.space) {
+    store.addSpaceState($page.params?.space, data.state);
+    store.stateReady();
+  }
+
+  $: if (data?.events) {
+    let roomID = data.events[0]?.room_id;
+    if (isIndex) {
+      roomID = "index";
     }
-    if(!$store.events[roomID]) {
-        $store.events[roomID] = {chat: [], board: []}
+    if (isAll) {
+      roomID = "all";
     }
-    $store.events[roomID].board = data.events
-}
-
-$: if(data?.messages) {
-    let roomID = data.messages[0]?.room_id
-    if(!$store.events[roomID]) {
-        $store.events[roomID] = {chat: [], board: []}
+    if (!$store.events[roomID]) {
+      $store.events[roomID] = { chat: [], board: [] };
     }
-    $store.events[roomID].chat = data.messages
-}
+    $store.events[roomID].board = data.events;
+  }
 
-$: if(data?.event) {
-    $store.post = data.event
-}
+  $: if (data?.messages) {
+    let roomID = data.messages[0]?.room_id;
+    if (!$store.events[roomID]) {
+      $store.events[roomID] = { chat: [], board: [] };
+    }
+    $store.events[roomID].chat = data.messages;
+  }
 
-$: isIndex = $page?.url.pathname === '/'
-$: isSpace = $page?.params?.space !== undefined 
-$: isPost = $page?.params?.post !== undefined 
-$: isContext = $page.url?.searchParams?.get('context') !== null
-$: isAll = $page.url?.pathname === '/all';
+  $: if (data?.event) {
+    $store.post = data.event;
+  }
 
-$: spaceExists = data?.state?.space !== undefined
+  $: isIndex = $page?.url.pathname === "/";
+  $: isSpace = $page?.params?.space !== undefined;
+  $: isPost = $page?.params?.post !== undefined;
+  $: isContext = $page.url?.searchParams?.get("context") !== null;
+  $: isAll = $page.url?.pathname === "/all";
 
-let showIndex = PUBLIC_INDEX === 'true'
+  $: spaceExists = data?.state?.space !== undefined;
 
+  let showIndex = PUBLIC_INDEX === "true";
 
-let ready = true
+  let ready = true;
 
-function viewReady() {
+  function viewReady() {
     ready = true;
-}
+  }
 
-let root;
+  let root;
 
+  $: down = $store.down;
 
-$: down = $store.down
+  $: menuToggled = $store.menuToggled;
 
+  function collapse() {
+    store.toggleMenu();
+  }
 
+  $: isStaticRoute =
+    $store.staticRoutes.filter((r) => $page.url.pathname.startsWith(r.path))
+      ?.length > 0;
 
-$: menuToggled = $store.menuToggled
+  $: if (isStaticRoute) {
+    ready = true;
+  }
 
-function collapse() {
-    store.toggleMenu()
-}
+  function switchToIndex() {
+    showIndex = true;
+  }
 
-$: isStaticRoute = $store.staticRoutes.filter(r =>
-$page.url.pathname.startsWith(r.path))?.length > 0
+  $: authenticated = $store?.authenticated && $store?.credentials != null;
+  $store?.credentials?.access_token?.length > 0;
 
+  let EmojiPicker;
+  let SpaceSettings;
+  let RoomSettings;
+  let VerificationAlert;
+  let Verification;
+  let UserSettings;
 
-
-$: if(isStaticRoute) {
-    ready = true
-}
-
-function switchToIndex() {
-    showIndex = true
-}
-
-$: authenticated = $store?.authenticated && 
-    $store?.credentials != null
-    $store?.credentials?.access_token?.length > 0
-
-let EmojiPicker;
-let SpaceSettings;
-let RoomSettings;
-let VerificationAlert;
-let Verification;
-let UserSettings;
-
-/*
+  /*
 $: if(authenticated && $store.loadEmojiPicker) {
     import('$lib/emoji/emoji-picker.svelte').then(m => {
         EmojiPicker = m.default
@@ -125,167 +144,173 @@ $: if(authenticated && $store.loadEmojiPicker) {
 }
 */
 
-$: if(authenticated) {
+  $: if (authenticated) {
+    import("$lib/emoji/emoji-picker.svelte").then((m) => {
+      EmojiPicker = m.default;
+    });
 
-    import('$lib/emoji/emoji-picker.svelte').then(m => {
-        EmojiPicker = m.default
-    })
+    import("$lib/space/settings/settings.svelte").then((m) => {
+      SpaceSettings = m.default;
+    });
+    import("$lib/room/settings/settings.svelte").then((m) => {
+      RoomSettings = m.default;
+    });
+    import("$lib/verification/alert.svelte").then((m) => {
+      VerificationAlert = m.default;
+    });
+    import("$lib/verification/verification.svelte").then((m) => {
+      Verification = m.default;
+    });
+    import("$lib/user/settings/settings.svelte").then((m) => {
+      UserSettings = m.default;
+    });
+  }
 
-    import('$lib/space/settings/settings.svelte').then(m => {
-        SpaceSettings = m.default
-    })
-    import('$lib/room/settings/settings.svelte').then(m => {
-        RoomSettings = m.default
-    })
-    import('$lib/verification/alert.svelte').then(m => {
-        VerificationAlert = m.default
-    })
-    import('$lib/verification/verification.svelte').then(m => {
-        Verification = m.default
-    })
-    import('$lib/user/settings/settings.svelte').then(m => {
-        UserSettings = m.default
-    })
-}
+  let loaded = false;
 
-let loaded = false;
-
-onMount(() => {
+  onMount(() => {
     loaded = true;
-})
+  });
 
-function setupSwipeGestures() {
-}
+  function setupSwipeGestures() {}
 
-let initialX = null;
-let initialY = null;
+  let initialX = null;
+  let initialY = null;
 
-function startTouch(e) {
+  function startTouch(e) {
     initialX = e.touches[0].clientX;
     initialY = e.touches[0].clientY;
-};
- 
-function moveTouch(e) {
+  }
+
+  function moveTouch(e) {
     if (initialX === null) {
-        return;
+      return;
     }
     if (initialY === null) {
-        return;
+      return;
     }
     let currentX = e.touches[0].clientX;
     let currentY = e.touches[0].clientY;
 
     let diffX = initialX - currentX;
     let diffY = initialY - currentY;
- 
+
     if (Math.abs(diffX) > Math.abs(diffY)) {
-        if (diffX > 0) {
-            if(menuToggled) {
-                collapse()
-            }
-        } else {
-            if(!menuToggled) {
-                store.toggleMenu()
-            }
-        }  
+      if (diffX > 0) {
+        if (menuToggled) {
+          collapse();
+        }
+      } else {
+        if (!menuToggled) {
+          store.toggleMenu();
+        }
+      }
     }
- 
+
     initialX = null;
     initialY = null;
 
     e.preventDefault();
-};
+  }
 
-$: spaceTitle = data?.space?.alias ? `${data?.space?.alias} -
-${PUBLIC_META_TITLE}` : PUBLIC_META_TITLE
+  $: spaceTitle = data?.space?.alias
+    ? `${data?.space?.alias} -
+${PUBLIC_META_TITLE}`
+    : PUBLIC_META_TITLE;
 
-$: fb = data?.event?.content?.body ? `${data?.event?.content?.body.slice(0, 1000)}...` : ''
+  $: fb = data?.event?.content?.body
+    ? `${data?.event?.content?.body.slice(0, 1000)}...`
+    : "";
 
-$: pb = data?.event?.content?.body?.length > 50 ?
-    `${data?.event?.content?.body.slice(0, 50)}...` :  
-    data?.event?.content?.body 
+  $: pb =
+    data?.event?.content?.body?.length > 50
+      ? `${data?.event?.content?.body.slice(0, 50)}...`
+      : data?.event?.content?.body;
 
-$: pt = data?.event?.content?.title ? data?.event?.content?.title :
-    pb ? pb : ''
+  $: pt = data?.event?.content?.title
+    ? data?.event?.content?.title
+    : pb
+    ? pb
+    : "";
 
-$: ct = data?.event?.type == 'm.room.message' ?
-    `${sender}: "${pt}"` : pt
+  $: ct = data?.event?.type == "m.room.message" ? `${sender}: "${pt}"` : pt;
 
-$: postTitle = pt ? `${ct} - ${PUBLIC_META_TITLE}` : PUBLIC_META_TITLE
+  $: postTitle = pt ? `${ct} - ${PUBLIC_META_TITLE}` : PUBLIC_META_TITLE;
 
-$: title = isIndex ? PUBLIC_META_TITLE : (isPost || isContext) ? postTitle : isSpace ?
-    spaceTitle : PUBLIC_META_TITLE
+  $: title = isIndex
+    ? PUBLIC_META_TITLE
+    : isPost || isContext
+    ? postTitle
+    : isSpace
+    ? spaceTitle
+    : PUBLIC_META_TITLE;
 
-$: hasImage = data?.event?.content?.attachments?.length > 0 &&
-    data?.event?.content?.attachments[0]?.type?.startsWith('image')
+  $: hasImage =
+    data?.event?.content?.attachments?.length > 0 &&
+    data?.event?.content?.attachments[0]?.type?.startsWith("image");
 
-$: imageKey = hasImage ? data?.event?.content?.attachments[0]?.key : ''
+  $: imageKey = hasImage ? data?.event?.content?.attachments[0]?.key : "";
 
-$: imageSRC = `${PUBLIC_MEDIA_URL}/${imageKey}`
+  $: imageSRC = `${PUBLIC_MEDIA_URL}/${imageKey}`;
 
-$: sender = data?.event?.sender?.display_name ?
-data?.event?.sender?.display_name : data?.event?.sender?.username
+  $: sender = data?.event?.sender?.display_name
+    ? data?.event?.sender?.display_name
+    : data?.event?.sender?.username;
 
-$: if(isIndex && browser) {
-    document.title = PUBLIC_META_TITLE
-}
-
+  $: if (isIndex && browser) {
+    document.title = PUBLIC_META_TITLE;
+  }
 </script>
 
-
 <svelte:head>
+  <title>{title}</title>
+  <meta property="og:title" content={title} />
 
-    <title>{title}</title>
-    <meta property="og:title" content={title} />
+  <link rel="canonical" href={PUBLIC_BASE_URL} />
+  {@html webManifest}
 
-<link rel="canonical" href={PUBLIC_BASE_URL} />
-
-    {#if isPost && hasImage && imageSRC}
-        <meta property="og:image" content={imageSRC} />
-    {/if}
-    {#if (isPost || isContext) && sender}
-        <meta name="author" content={sender} />
-    {/if}
-    {#if !isSpace || (isSpace && !spaceExists)}
-        <meta property="og:image" content={PUBLIC_META_IMAGE || PUBLIC_FAVICON} />
-    {/if}
-    {#if (isPost || isContext) && data?.event}
-        <meta name="description" content={fb}>
-        <meta property="og:description" content={fb}>
-    {/if}
-    {#if isIndex || (isSpace && !spaceExists)}
-        <meta name="description" content={PUBLIC_META_DESCRIPTION}>
-        <meta property="og:description" content={PUBLIC_META_DESCRIPTION}>
-    {/if}
+  {#if isPost && hasImage && imageSRC}
+    <meta property="og:image" content={imageSRC} />
+  {/if}
+  {#if (isPost || isContext) && sender}
+    <meta name="author" content={sender} />
+  {/if}
+  {#if !isSpace || (isSpace && !spaceExists)}
+    <meta property="og:image" content={PUBLIC_META_IMAGE || PUBLIC_FAVICON} />
+  {/if}
+  {#if (isPost || isContext) && data?.event}
+    <meta name="description" content={fb} />
+    <meta property="og:description" content={fb} />
+  {/if}
+  {#if isIndex || (isSpace && !spaceExists)}
+    <meta name="description" content={PUBLIC_META_DESCRIPTION} />
+    <meta property="og:description" content={PUBLIC_META_DESCRIPTION} />
+  {/if}
 </svelte:head>
 
-
 {#if authenticated && EmojiPicker}
-    <svelte:component this={EmojiPicker} />
+  <svelte:component this={EmojiPicker} />
 {/if}
 
 {#if authenticated && SpaceSettings}
-    <svelte:component this={SpaceSettings} />
+  <svelte:component this={SpaceSettings} />
 {/if}
 
 {#if authenticated && RoomSettings}
-    <svelte:component this={RoomSettings} />
+  <svelte:component this={RoomSettings} />
 {/if}
 
 {#if authenticated && VerificationAlert}
-    <svelte:component this={VerificationAlert} />
+  <svelte:component this={VerificationAlert} />
 {/if}
 
 {#if authenticated && UserSettings}
-    <svelte:component this={UserSettings} />
+  <svelte:component this={UserSettings} />
 {/if}
-
 
 {#if authenticated && Verification}
-    <svelte:component this={Verification} />
+  <svelte:component this={Verification} />
 {/if}
-
-
 
 <Sync />
 <Health />
@@ -300,68 +325,52 @@ $: if(isIndex && browser) {
 <Settings />
 
 {#if !down}
+  {#if menuToggled}
+    <div class="maskcontent" on:click={collapse} />
+  {/if}
 
-
-{#if menuToggled}
-<div class="maskcontent" on:click={collapse}>
-</div>
-{/if}
-
-<div class="root" bind:this={root}>
-
+  <div class="root" bind:this={root}>
     <div class="container">
+      <div class="inner-container grd" class:show={menuToggled}>
+        <Switcher />
+        <Sidebar />
+      </div>
 
-        <div class="inner-container grd" class:show={menuToggled}>
-            <Switcher />
-            <Sidebar />
-        </div>
-
-        <div class="content" class:slide-in={menuToggled}>
-            {#if isStaticRoute}
-                <slot></slot>
-            {:else if isSpace && !spaceExists}
-                <NotFound />
-            {:else}
-                <View on:ready={viewReady} />
-            {/if}
-        </div>
-
+      <div class="content" class:slide-in={menuToggled}>
+        {#if isStaticRoute}
+          <slot />
+        {:else if isSpace && !spaceExists}
+          <NotFound />
+        {:else}
+          <View on:ready={viewReady} />
+        {/if}
+      </div>
     </div>
+  </div>
 
+  <Authentication />
 
-</div>
-
-
-
-
-<Authentication />
-
-    <CreateSpace />
-
-
-
+  <CreateSpace />
 {/if}
 
-
-{#if (isIndex && showIndex) || (!isIndex)}
-<section class="loading" class:hide={ready || down}>
+{#if (isIndex && showIndex) || !isIndex}
+  <section class="loading" class:hide={ready || down}>
     <section class="grd-c">
-        <div class="loader"></div>
+      <div class="loader" />
     </section>
-</section>
+  </section>
 {/if}
-
 
 {#if down}
-    <Down />
+  <Down />
 {/if}
 
-
-
+{#await import("$lib/components/reload-prompt.svelte") then { default: ReloadPrompt }}
+  <ReloadPrompt />
+{/await}
 
 <style>
-
-.loading {
+  .loading {
     z-index: 3000;
     top: 0;
     bottom: 0;
@@ -374,13 +383,13 @@ $: if(isIndex && browser) {
     display: grid;
     grid-template-columns: 100%;
     grid-template-rows: 100%;
-}
+  }
 
-.hide {
+  .hide {
     display: none;
-}
+  }
 
-.root {
+  .root {
     position: absolute;
     height: 100%;
     width: 100%;
@@ -388,9 +397,9 @@ $: if(isIndex && browser) {
     grid-template-columns: 100%;
     grid-template-rows: 100%;
     overflow: hidden;
-}
+  }
 
-.container {
+  .container {
     place-self: stretch;
     width: 100%;
     height: 100%;
@@ -400,16 +409,16 @@ $: if(isIndex && browser) {
     grid-template-rows: auto;
     grid-template-columns: [sidebar] 298px [content] 1fr;
     overflow: hidden;
-}
+  }
 
-.inner-container {
+  .inner-container {
     display: grid;
     grid-template-rows: auto;
     grid-template-columns: [switcher] 58px [sidebar] 240px;
     overflow: hidden;
-}
+  }
 
-.content {
+  .content {
     place-self: stretch;
     width: 100%;
     height: 100%;
@@ -419,17 +428,16 @@ $: if(isIndex && browser) {
     overflow: hidden;
     left: 0;
     background-color: var(--bg);
-}
+  }
 
-.slide-in {
+  .slide-in {
     position: fixed;
     top: 0;
     bottom: 0;
     left: 298px;
-}
+  }
 
-
-.maskcontent {
+  .maskcontent {
     position: fixed;
     top: 0;
     bottom: 0;
@@ -437,22 +445,18 @@ $: if(isIndex && browser) {
     height: 100%;
     z-index: 10000;
     width: 100vw;
-}
+  }
 
-
-
-@media screen and (max-width: 768px) {
+  @media screen and (max-width: 768px) {
     .container {
-        grid-template-columns: auto;
+      grid-template-columns: auto;
     }
 
     .inner-container {
-        display: none;
+      display: none;
     }
     .show {
-        display: grid;
+      display: grid;
     }
-
-}
-
+  }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,48 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 export default defineConfig({
-	plugins: [sveltekit()],
-    server: {
-        fs: {
-              allow: ['..'],
-        },
+  plugins: [
+    sveltekit(),
+    SvelteKitPWA({
+      manifest: {
+        name: "Commune",
+        short_name: "Commune",
+        background_color: "#fff",
+        theme_color: "#000",
+        start_url: "/",
+        display: "standalone",
+        prefer_related_applications: true,
+        icons: [
+          {
+            src: "/maskable_icon_x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/maskable_icon_x192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "any",
+          },
+          {
+            src: "/maskable_icon_x512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    fs: {
+      allow: [".."],
     },
-    build: {
-        minify: 'esbuild',
-        cssMinify: true,
-        sourcemap: true
+  },
+  build: {
+    minify: "esbuild",
+    cssMinify: true,
+    sourcemap: true,
   },
 });
